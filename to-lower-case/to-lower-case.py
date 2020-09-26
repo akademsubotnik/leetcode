cf422aad72c87bda9575ce618e19b3fd720ab7fd
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-09-26 11:22:21 -0400

    Create to-lower-case.py

---
+++ to-lower-case/to-lower-case.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        counter = 0
+        str_return = []
+        #while counter < len(str) :
+        #    if str[counter].isupper() :
+        #        print "IS UPPER"
+        #    counter +=1
+        
+        for counter in range(len(str)) :
+            if str[counter].isupper() :
+                #print "is upper"
+                str_return.append(str[counter].lower())
+            else :
+                str_return.append(str[counter])
+        #return str_return
+        
+        str_3 = ""
+        for counter in range(len(str_return)) :
+            str_3 = str_3 + str_return[counter]
+        return str_3
