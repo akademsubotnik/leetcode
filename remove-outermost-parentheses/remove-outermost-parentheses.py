6cefcdcde0fec26305bb8f88fd3feb6541969855
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-09-26 22:27:50 -0400

    Update remove-outermost-parentheses.py

--- remove-outermost-parentheses/remove-outermost-parentheses.py
+++ remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -4,19 +4,16 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
+        #Step 1: Remove outermost parentheses
+        str_return = ""
+        str_return = S[1:len(S) - 1]
         
         #If next parentheses is not different, remove it
-        #print S
-        str_return = ""
         
-        for counter in range(len(S) - 1):
-            if S[counter] != S[counter + 1] :
-                str_return = str_return + S[counter]
-        
-        if str_return[len(str_return) - 1] != ')' :
-            str_return = str_return + ')'
-        
-        print str_return
+        #for counter in range (len(str_return)) :
+        counter = 0
+        while counter < (len(str_return) - 1) :
+            if str_return[counter] == str_return[counter + 1] :
+                str_return = str_return[0:counter] + str_return[counter+1:]
+            counter += 1
         return str_return
-            
-        #()()()()(())
