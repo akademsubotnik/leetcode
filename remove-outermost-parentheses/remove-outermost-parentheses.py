a8601f85e9df62bfef01e4314d5cd5069e8d2728
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-01 11:34:39 -0400

    Update remove-outermost-parentheses.py

--- remove-outermost-parentheses/remove-outermost-parentheses.py
+++ remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -7,13 +7,55 @@ class Solution(object):
         #Step 1: Remove outermost parentheses
         str_return = ""
         str_return = S[1:len(S) - 1]
+                
+        #Input: "(()())(())(()(()))" Output: "()()()()()" Expected: "()()()()(())"
+        #Input: "(()())(())" Expected: "()()()"
+        # Step 2. Count LP/RP and make sure they are equal, if they are not equal, remove the correct LP or RP
+        numlp,numrp = 0,0
         
-        #If next parentheses is not different, remove it
+        for counter in range (len(str_return)) :
+            #If character is ( increment left parentheses counter
+            if str_return[counter] == '(' :
+                numlp += 1
+            #There are only ( and ) if the string, if the character is not a (, it must be a )
+            else :
+                numrp += 1
+        #Remove errant right parentheses     
+        print "Len str_return: " + str(len(str_return))
+        for counter in range (len(str_return) - 1) :
+            print str(counter) + " " + str_return[counter]
+            if counter == 0 :
+                if (str_return[counter] == ')') :
+                    #Remove right parentheses at str_return[0]
+                    str_return = str_return[:counter] +  str_return[counter+1:]
+            elif str_return[counter] == ')' and str_return[counter - 1] != '('  :
+                #Remove the errant right parentheses )
+                str_return = str_return[:counter] +  str_return[counter+1:]
         
-        #for counter in range (len(str_return)) :
-        counter = 0
-        while counter < (len(str_return) - 1) :
-            if str_return[counter] == str_return[counter + 1] :
-                str_return = str_return[0:counter] + str_return[counter+1:]
-            counter += 1
+        
+        #Start at last index of string and then check going backward if there is a ( without a preceding )
+        
+        #print str_return
+        counter01 = len (str_return) - 1
+        while counter01 >= 0 :
+            if str_return[counter01] == '(' and str_return[counter01 + 1] != ')' :
+                #Remove the errant left parentheses (
+                str_return = str_return[:counter01] +  str_return[counter01+1:]
+            #print str_return[counter]
+            counter01 -= 1
         return str_return
+        
+        #txt = "Hello World"[::-1]
+        #str_return_reversed = str_return[::-1]
+        #print "str_return: " + str_return
+        #print "str_return_reversed: " + str_return_reversed
+        
+        
+        #print "numlp: " + str(numlp)
+        #print "numrp: " + str(numrp)
+        #print "str_return:" + str_return
+        
+        
+        # "(()())(())"
+        # ()())(()
+        # )(())()(
