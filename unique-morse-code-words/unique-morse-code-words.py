4dfddada685e87995126dfe1df7d81f26a6d39b1
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-02 15:24:34 -0400

    Update unique-morse-code-words.py

--- unique-morse-code-words/unique-morse-code-words.py
+++ unique-morse-code-words/unique-morse-code-words.py
@@ -5,18 +5,6 @@ class Solution(object):
         :rtype: int
         """
         
-        #g : --.
-        #i : ..
-        #n : -.
-        
-        #--. .. -.
-        
-        #z : --..
-        #e : .
-        #n : -.
-        
-        #--.. . -.
-        
         alphabet_dict = {"a":".-", "b":"-...", "c":"-.-.","d":"-..","e":".","f":"..-.","g":"--.","h":"....","i":"..","j":".---","k":"-.-","l":".-..","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-","u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--.."};
 
         morse_array = []
@@ -26,11 +14,7 @@ class Solution(object):
                 tmp_char =  alphabet_dict.get(j)
                 tmp_str = tmp_str + tmp_char
             morse_array.append(tmp_str)
-              
-        #print morse_array
-        #print words
-        
+                      
         #check unique values in morse_array
-        #for i in (morse_array) :
         myset = set(morse_array) 
         return len(myset)
