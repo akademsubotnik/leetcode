4d68b1008989519812c0f5d1557356a7dae57c56
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-02 12:22:04 -0400

    Update number-of-students-doing-homework-at-a-given-time.py

--- number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
+++ number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
@@ -6,18 +6,11 @@ class Solution(object):
         :type queryTime: int
         :rtype: int
         """
-        
-        working_return = []
-        # If starttime[i] is less than or equal AND endtime[i] is greater than or equal to querytime
-            #return true
-        # else
-            #return false
-        
-        i = 0
+        working_return,i = 0,0
         
         while i < len(endTime) :
             if startTime[i] <= queryTime and endTime[i] >= queryTime :
-                working_return.append(i)
+                working_return += 1
             i += 1
         
-        return len(working_return)
+        return working_return
