c10659331e941e9bef28fe0e0683b3f23610a802
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-03 20:44:49 -0400

    Update find-n-unique-integers-sum-up-to-zero.py

--- find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
+++ find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
@@ -7,15 +7,11 @@ class Solution(object):
         """
         #If n is an odd number, return n - 1 numbers and 0, where half are positve and the other half are the negative version of that number
         #If n is an even number, return n numbers, where half are positve and the other half are the negative version of that number
-        #print n
+
         
-        #print math.floor( 7/2 )
-        
-        return_array = []
-        
-        counter = 1
+        return_array,counter = [],1
+
         while counter <= int(math.floor(n/2)) :
-            #print counter
             return_array.append(counter)
             return_array.append(counter * -1)
             counter += 1
@@ -24,6 +20,3 @@ class Solution(object):
             return_array.append(0)
         
         return list(return_array)
-        
-        #for i in range(int(math.floor(n/2))):
-        #    print i
