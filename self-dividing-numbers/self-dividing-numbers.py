186f3a1c815809bea01967b92f39810ecc52def0
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-07 14:36:22 -0400

    Update self-dividing-numbers.py

--- self-dividing-numbers/self-dividing-numbers.py
+++ self-dividing-numbers/self-dividing-numbers.py
@@ -5,33 +5,25 @@ class Solution(object):
         :type right: int
         :rtype: List[int]
         """
+        #Remove elements with 0
         dump_array = []
         for i in range(left,right+1) :
             if '0' not in str(i):
                 dump_array.append(i)
-        #print dump_array
         
+        #Add numbers that divide by that comprise them (will contain duplicates/numbers that are only divisible by one of the numbers that comprise them)
         divisible_array = []
         for i in dump_array:
-            #print i
             for j in range(0,len(str(i))) :
-                #print str(i)
                 if i % int(str(i)[j]) == 0 :
                     divisible_array.append(i)
-        #print divisible_array
         
         #Go through the array and check if the number of entries for the number is equal to the number length, if it is add it to the next array, if it is not, do not add it to the next array
         
         array_removeduplicates = []
         for i in divisible_array : 
-            #print i
-            #print divisible_array.count(i)
             if divisible_array.count(i) == len(str(i)) :
                 array_removeduplicates.append(i)
-        
-        #tmp_list = list(set(array_removeduplicates))
-        
-        #print array_removeduplicates
         
         res = [] 
         for i in array_removeduplicates : 
