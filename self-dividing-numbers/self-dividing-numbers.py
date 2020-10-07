7140b28f98fffa105836a8c54a9dc8f8d3d46bfb
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-07 14:41:26 -0400

    Update self-dividing-numbers.py

--- self-dividing-numbers/self-dividing-numbers.py
+++ self-dividing-numbers/self-dividing-numbers.py
@@ -19,12 +19,12 @@ class Solution(object):
                     divisible_array.append(i)
         
         #Go through the array and check if the number of entries for the number is equal to the number length, if it is add it to the next array, if it is not, do not add it to the next array
-        
         array_removeduplicates = []
         for i in divisible_array : 
             if divisible_array.count(i) == len(str(i)) :
                 array_removeduplicates.append(i)
         
+        #Put numbers in ascending order
         res = [] 
         for i in array_removeduplicates : 
             if i not in res: 
