9c1bd300dab41deaa5e804e0cb20677e606daae4
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-10 19:43:54 -0400

    Update hamming-distance.py

--- hamming-distance/hamming-distance.py
+++ hamming-distance/hamming-distance.py
@@ -5,31 +5,24 @@ class Solution(object):
         :type y: int
         :rtype: int
         """
-        z0 = "{0:b}".format(x)
-        z1 = "{0:b}".format(y)
-        int_difference = 0
+        z0,z1,int_difference = "{0:b}".format(x),"{0:b}".format(y),0
+        list_z0,list_z1 = list(z0),list(z1)
         
-        list_z0 = list(z0)
-        list_z1 = list(z1)
         
         if len(list_z0) > len(list_z1) :
             #If z0 is larger than z1
             while len(list_z0) != len(list_z1) :
-                list_z1.insert(0,0)
+                list_z1.insert(0,'0')
         if len(list_z0) < len(list_z1) :
             while len(list_z0) != len(list_z1) :
                 #If z1 is larger than z0
-                list_z0.insert(0,0)
+                list_z0.insert(0,'0')
 
-        int_len = len(list_z0)
-        #print int_len
         
         i = 0
-        while i < int_len :
-            #print list_z0[i], type(list_z0[i]) , list_z1[i],type(list_z1[i])
-            if int(list_z0[i]) != int(list_z1[i]) :
-                int_difference += 1
-            
+        while i < len(list_z0) :
+            if list_z0[i] != list_z1[i] :
+                int_difference += 1       
             i += 1
         
         return int_difference
