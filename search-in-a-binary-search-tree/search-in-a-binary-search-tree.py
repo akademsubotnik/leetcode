ecdaca728d06cd80589ba88dddf211975d27e224
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-10-10 00:57:15 -0400

    Update search-in-a-binary-search-tree.py

--- search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -11,29 +11,17 @@ class Solution(object):
         :type val: int
         :rtype: TreeNode
         """
-        #Use the pre-order traversal process which you used in the mergeTrees problem (root->left subtree->right subtree)
         
         t = root 
-        t_t = root
-        #print t.val
 
         #If val is less than root.val -> to left subtree, set root = node left of the root node, check again is val less than root.val if yes, move to left subtree, set root = node left of root node, check again, is val less than root.val if no move to right subtree, set root = node right of root node, check again.  repeat until val = root.val or you reach no next node
         
         while t != None :
-            print t.val
             if val == t.val :
-                print "val found"
-                print t
                 return t
-            if val < t.val :
+            elif val < t.val :
                 #Move to left subtree
-                print "Moving to left subtree"
                 t = t.left
             elif val > t.val :
                 #Move to right subtree
-                print "Moving to right subtree"
                 t = t.right
-            
-            
-        
-        
