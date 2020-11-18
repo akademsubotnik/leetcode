b416700a63d2f72c394613db88a942c0d3c0c69a
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-11-18 12:10:19 -0500

    Update Increasing-Order-Search-Tree.py

--- 897_Increasing-Order-Search-Tree/Increasing-Order-Search-Tree.py
+++ 897_Increasing-Order-Search-Tree/Increasing-Order-Search-Tree.py
@@ -1,1 +1,27 @@
-#Create IOST.py
+#--------------------------------------------------------------------------------------------
+#  Copyright (c) Red Hat, Inc. All rights reserved.
+#  Licensed under the MIT License. See LICENSE in the project root for license information.
+#--------------------------------------------------------------------------------------------
+
+"""
+897. Increasing Order Search Tree
+Given the root of a binary search tree, rearrange the tree in in-order so that the 
+leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+                    |5|                     |1|
+                |3|    |6|        -->         |2|
+              |2| |4|    |8|                    |3|
+            |1|        |7| |9|                    |4|
+                                                    |5|
+                                                      |6|
+                                                        |7|
+                                                          |8|
+                                                            |9|
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+
+"""
+
+Hello = "hello"
+print (Hello)
