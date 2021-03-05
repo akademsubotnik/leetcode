2f7fb9b3624dd5461c8c8c236fddbb15c8c013ba
Greg Koval <44036625+gregg00@users.noreply.github.com>
2021-03-05 17:05:28 +0200

    Update arithmeticoperations.sh

--- bash/HackerRank/arithmeticoperations/arithmeticoperations.sh
+++ bash/HackerRank/arithmeticoperations/arithmeticoperations.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#numberone contains the entire expression
+#echo "numberone ", $numberone
+
+#declare -i numberone
+read numberone
+
+#Will replace spaces with C
+#echo $numberone | tr " " "C" | tr "\t" "&"
+
+#Will replace spaces with nothing
+#echo $numberone | awk '{ gsub(/ /,""); print }'
+#echo $(($numberone)) | bc -l
+
+echo 'scale=3;' $numberone | bc
