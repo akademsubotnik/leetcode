c592039a635e58329dff0c42377aa1c2b9510ef1
Greg Koval <44036625+gregg00@users.noreply.github.com>
2021-03-06 16:10:03 +0200

    Update computetheaverage.sh

--- bash/HackerRank/computetheaverage/computetheaverage.sh
+++ bash/HackerRank/computetheaverage/computetheaverage.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+#read first value to get total number of passed numbers
+read numbers
+echo $numbers
+
+START=1
+SUM=0
+
+for (( c=$START; c<=$numbers; c++ ))
+do
+  read value
+  #SUM = $SUM + $
+  #num=$(($num1 + $num2))
+  SUM=$(($SUM + $value))
+done
+
+echo $(($SUM / 100))
